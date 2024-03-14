9af36a01d83054fd0dd5ed074141ca350254a80c
Muku <44918265+muku314115@users.noreply.github.com>
2024-03-14 14:54:37 +0530

    Update settings.go

--- config/settings.go
+++ config/settings.go
@@ -16,7 +16,7 @@ type Settings struct {
 }
 
 func LoadConfig() {
-	//time.Sleep(10 * time.Second)
+	time.Sleep(10 * time.Second)
 	file, err := os.Open(strings.TrimSuffix(os.Getenv("CONFIG_PATH"), "/") + "/config/settings.json")
 	//file, err := os.Open("/Users/mukundrawat/powerloom/proto-snapshot-server/config/settings.json")
 	if err != nil {
