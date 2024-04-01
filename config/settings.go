65aaddf9891ef5be40800b168bb81cfce831456d
muku314115 <mukangotan@gmail.com>
2024-04-01 14:32:20 +0530

    removing irrelevant code

--- config/settings.go
+++ config/settings.go
@@ -1,12 +1,10 @@
 package config
 
 import (
-	"bufio"
 	"encoding/json"
 	log "github.com/sirupsen/logrus"
 	"os"
 	"strings"
-	"time"
 )
 
 var SettingsObj *Settings
@@ -17,7 +15,6 @@ type Settings struct {
 }
 
 func LoadConfig() {
-	time.Sleep(10 * time.Second)
 	file, err := os.Open(strings.TrimSuffix(os.Getenv("CONFIG_PATH"), "/") + "/config/settings.json")
 	//file, err := os.Open("/Users/mukundrawat/powerloom/proto-snapshot-server/config/settings.json")
 	if err != nil {
@@ -37,31 +34,5 @@ func LoadConfig() {
 		log.Fatalf("Failed to decode config file: %v", err)
 	}
 
-	file, err = os.Open("/shared_data/relayer_id.txt")
-	if err != nil {
-		log.Debugf("Error opening relayer info file: %v", err)
-	}
-	defer file.Close()
-
-	// Initialize variables to hold relayer URL and ID
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		config.RelayerId = line
-	}
-	file, err = os.Open("/shared_data/relayer_url.txt")
-	if err != nil {
-		log.Debugf("Error opening relayer info file: %v", err)
-	}
-	defer file.Close()
-
-	// Initialize variables to hold relayer URL and ID
-
-	scanner = bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		config.RelayerUrl = line
-	}
 	SettingsObj = &config
 }
